3cf990214b50aa3d1fb93e6f130d09476d6c4a1a
sanjaymajji <124818525+sanjaymajji@users.noreply.github.com>
2023-07-30 21:30:43 +0530

    Add files via upload

---
+++ frontend/src/components/Reg.jsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import axios from "axios"
+
+const Reg =()=> {
+
+  let ship=useNavigate()
+  
+  const [gmail,setgmail]= useState();
+  const [password,setpassword]= useState();
+  const [test,settest]=useState(0)
+  
+function submit(e){
+
+  e.preventDefault()
+
+
+  axios.post("http://localhost:3090",{
+    gmail,password
+  }).then((res)=>{
+    let a=res.data
+    
+    if("done buddy"==a.mess){
+     ship("/home")
+    }
+    else{
+      alert("invail user id or password")
+    }
+  }).catch((err)=>{
+    console.log(err)
+    settest("err")
+  })
+}
+
+
+
+ 
+  return (
+    <>
+    <p>{test}</p>
+    <h1>QuizHub</h1>
+     <form method="POST" onSubmit={submit}>
+      
+      <label >GMAIL</label>
+      <input type="text" name='mail_data'onChange={(e)=>{
+        setgmail(e.target.value)
+      }}/>
+      
+      
+
+<label >PASSWORD</label>
+      <input type="text" name='name_data' onChange={(e)=>{
+        setpassword(e.target.value)
+      }}/>
+
+    <button type='submit'>
+     
+
+    login
+
+    </button>
+    
+
+    </form>
+    {/* <button>
+      <Link to="/home">login</Link>
+    </button> */}
+    <div className='dumbstars'>
+      <p>new here</p>
+      <button>
+      <Link to="/register">register</Link>
+    </button>
+      
+    </div>
+    
+    </>
+    
+  );
+};
+
+export {Reg};
+
+
+
+
+
