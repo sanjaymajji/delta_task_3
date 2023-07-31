57cb6e52c756012afbe30b1db173bdc81a1a3e46
sanjaymajji <124818525+sanjaymajji@users.noreply.github.com>
2023-07-31 23:38:29 +0530

    Add files via upload

---
+++ frontend_react/src/components/Login_pg.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react'
+import { Link,useNavigate } from 'react-router-dom'
+import axios from "axios"
+
+
+const Login=()=> {
+  const ship=useNavigate()
+  const [name,setname]= useState();
+  const [gmail,setgmail]= useState();
+  const [gender,setgender]= useState();
+  const [interest,setinterest]= useState();
+  const [password,setpassword]= useState();
+  
+
+  function confrim_submit (e){
+    e.preventDefault()
+    
+    axios.post("http://localhost:3090/register",{
+      name,gmail,gender,interest,password
+    }).then((res)=>{
+      if(res.data=="done"){
+        ship("/")
+      }
+      else{
+        alert("something went wrong")
+      }
+    })
+    
+
+
+
+
+  }
+  return (
+    <>
+    <h1>QuizHub</h1>
+
+    
+     <form method="POST" onSubmit={confrim_submit}>
+      <label >NAME</label>
+      <input type="text" name='name_data' onChange={(e)=>{
+        setname(e.target.value)
+      }}/>
+
+      <label >GMAIL</label>
+      <input type="text" name='mail_data'onChange={(e)=>{
+        setgmail(e.target.value)
+      }}/>
+      
+      <label >GENDER</label>
+      <input type="text" name='gender_data'onChange={(e)=>{
+        setgender(e.target.value)
+      }}/>
+      
+      <label >INTEREST</label>
+      <input type="text" name='interest_data'onChange={(e)=>{
+        setinterest(e.target.value)
+      }}/>
+
+<label >PASSWORD</label>
+      <input type="text" name='password_data' onChange={(e)=>{
+        setpassword(e.target.value)
+      }}/>
+
+
+    <button type='submit'>
+     
+
+    confirm
+
+    </button>
+
+    </form>
+    {/* <button>
+      <Link to="/home">login</Link>
+    </button> */}
+
+    <div className='dumbstars'>
+      <p>already have accout</p>
+      <button>
+      <Link to="/">log in</Link>
+    </button>
+      
+    </div>
+    
+    </>
+    
+  );
+};
+
+export {Login};
